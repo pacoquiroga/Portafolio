cb7baf1538f0d143caefb7bea41218299a33ab23
Francisco Quiroga <fsquiroga2@espe.edu.ec>
2024-05-20 23:02:22 -0500

    NAV RESPONSIVE

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-window.onscroll = function() {
+window.onscroll = function () {
     if (document.documentElement.scrollTop > 100) {
         document.querySelector('.go-top-container').classList.add('show');
     }
@@ -12,4 +12,22 @@ document.querySelector('.go-top-container').addEventListener('click', () => {
         top: 0,
         behavior: 'smooth'
     });
+});
+
+// Selecciona el checkbox
+var checkbox = document.getElementById('check');
+
+// Selecciona el ul que contiene los elementos del nav
+var navList = document.querySelector('.nav');
+
+// Añade un event listener para el evento 'change' del checkbox
+checkbox.addEventListener('change', function() {
+    // Si el checkbox está marcado (checked)
+    if (checkbox.checked) {
+        // Agrega la clase 'show' al ul para mostrarlo
+        navList.classList.add('show');
+    } else {
+        // Si el checkbox no está marcado, elimina la clase 'show' del ul para ocultarlo
+        navList.classList.remove('show');
+    }
 });
